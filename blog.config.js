c5174b7da2608ddb2e034282f4913e2e29f3a5c0
Shawn Ma <86111037+ShawnMa123@users.noreply.github.com>
2022-08-01 23:59:57 +0800

    Update blog.config.js
    update author and title

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
+  title: 'Shawn_Blog',
+  author: 'Shawn Ma',
+  email: 'xxx',
+  link: 'shawn.fsyl.tk',
   description: 'This gonna be an awesome website.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
